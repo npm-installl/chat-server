5077fa0b36423c9a4296c36350f0997b76640952
npm-installl <samkk7051@gmail.com>
2024-07-28 12:54:56 +0530

    initial commit

---
+++ models/message.js
@@ -0,0 +1,12 @@
+// models/message.js
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  room: String,
+  user: String,
+  message: String,
+  timestamp: { type: Date, default: Date.now }
+});
+
+const Message = mongoose.model('Message', messageSchema);
+module.exports = Message;
